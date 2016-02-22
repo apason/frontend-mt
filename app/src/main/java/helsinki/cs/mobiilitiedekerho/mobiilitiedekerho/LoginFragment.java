b0290cb79f4086a69444f9f0b04617f7680ecbfc
henrydav <Henry@Maija-Air.lan>
2016-02-23 00:17:51 +0200

    Jatkettu kirjautumisen toteutusta

--- app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/LoginFragment.java
+++ app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/LoginFragment.java
@@ -1,15 +1,20 @@
 package helsinki.cs.mobiilitiedekerho.mobiilitiedekerho;
 
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Point;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
@@ -19,88 +24,82 @@ import android.widget.Toast;
 public class LoginFragment extends Fragment implements View.OnClickListener {
 
     View view;
-    PopupWindow loginPopup;
     TextView emailTV;
     TextView passwordTV;
-
-
+    String email;
+    String password;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.login_button_fragment, container, false);
+        view = inflater.inflate(R.layout.login_button_fragment, null);
         // Add onClickListener to the login button
-        ImageButton loginButton =
+        ImageButton loginIconButton =
                 (ImageButton) view.findViewById(R.id.login_icon_button);
-        loginButton.setOnClickListener(this);
+        loginIconButton.setOnClickListener(this);
 
         return view;
     }
 
-    // When loginButton is pressed call method openLoginPopup
+    // When loginButton is pressed call method openLoginDialog
     @Override
     public void onClick(View v) {
-        openLoginPopup();
+        openLoginDialog();
     }
 
-    private void openLoginPopup() {
-        /* REMOVED TEMPORARILY BECAUSE OF A BUG
-        // Inflate the popup_layout.xml
-        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.login_fragment, null);
-        RelativeLayout layout = (RelativeLayout) view.findViewById(R.id.login_fragment);
+    private void openLoginDialog() {
+        final Dialog login = new Dialog(LoginFragment.this.getActivity());
+        // Set GUI of login screen
+        login.setContentView(R.layout.login_fragment);
+        login.setTitle("Kirjaudu mobiilitiedekerhoon");
 
-        // Request loginPopup to be drawn to the center of the screen
-        loginPopup = new PopupWindow(layout);
-        loginPopup.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-        // Add TextView elements to the screen.
+        // Add TextView elements to the dialog
         emailTV = (TextView) view.findViewById(R.id.username);
         passwordTV = (TextView) view.findViewById(R.id.password);
 
-        // Add buttons to the popup and set onclicklisteners
+        // Add buttons to the dialog and set onclicklisteners
         Button loginButton =
-                (Button) view.findViewById(R.id.login_button);
-        loginButton.setOnClickListener(login_listener);
+                (Button) login.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO On click of login button get content from TextViews and check if they match a valid user using ServerCommunication.
+                email = emailTV.getText().toString();
+                password = passwordTV.getText().toString();
 
-        Button closePopupButton =
-                (Button) view.findViewById(R.id.cancel_button);
-        closePopupButton.setOnClickListener(close_popup_listener);
-*/
-    }
-
-    private OnClickListener login_listener = new OnClickListener() {
-        public void onClick(View v) {
-            // On click of login button get content from TextViews and check if they match a valid
-            // user using ServerCommunication.
-            String email = emailTV.getText().toString();
-            String password = passwordTV.getText().toString();
-            /*
-            ServerCommunication servcom = new ServerCommunication();
-            if (servcom.AuthenticateUser(email, password) == true) {
-                Toast.makeText(LoginFragment.this.getActivity(), "Kirjautuminen onnistui!",
-                        Toast.LENGTH_LONG).show();
-                loginPopup.dismiss();
-            } else {
-                // If username or password is incorrect empty TextViews and notify user.
-                emailTV.setText("");
-                passwordTV.setText("");
-                Toast.makeText(LoginFragment.this.getActivity(), "Sähköpostiosoite tai salasana väärin!",
-                        Toast.LENGTH_LONG).show();
+                ServerCommunication servcom = new ServerCommunication();
+                if (servcom.AuthenticateUser(email, password) == true) {
+                    Toast.makeText(LoginFragment.this.getActivity(), "Kirjautuminen onnistui!",
+                            Toast.LENGTH_LONG).show();
+                    login.dismiss();
+                } else {
+                    // TODO If username or password is incorrect empty TextViews and notify user.
+                    emailTV.setText("");
+                    passwordTV.setText("");
+                    Toast.makeText(LoginFragment.this.getActivity(), "Sähköpostiosoite tai salasana väärin!",
+                            Toast.LENGTH_LONG).show();
             }
-            */
-        }
-    };
+        }});
 
-    // On click of close button close the popup.
-    private OnClickListener close_popup_listener = new OnClickListener() {
-        public void onClick(View v) {
-            loginPopup.dismiss();
-        }
-    };
+        // On click of cancel button close the dialog
+        Button closePopupButton =
+                (Button) login.findViewById(R.id.cancel_button);
+        closePopupButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login.dismiss();
+                }
 
+        });
 
-
+        // Force the dialog to the right size
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(login.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+        login.show();
+        login.getWindow().setAttributes(lp);
+    }
 
 }
