7277f71a0f07aa622adc505b648b8d46bd6fd7d3
Daniel <daniel.martinkarttunen@helsinki.fi>
2016-03-01 01:04:35 +0200

    Uusi serveri kommunikaatio rakenne, muuten tehty paitsise pointti mitä pitää päättää ryhmässä ja siihen liittyvä työ. (+nyt activityen on hoidettava kaikki mitä nyt ei ole enää näissä)

--- app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/StatusService.java
+++ app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/StatusService.java
@@ -4,11 +4,18 @@ package helsinki.cs.mobiilitiedekerho.mobiilitiedekerho;
 public class StatusService {
 
     public static class StaticStatusService {
+    
+        protected static final String urli = "https://mobiilitiedekerho.duckdns.org:27461/"; //The IP of the back-end server, it is needed to add parameters to it to be able to comunivate with it. Hard-coded.
+    
         protected static boolean loggedIn;
         protected static String nick;
+        protected static String token;
+        
+        private final JsonConverter jc = new JsonConverter();
+        private final ServerCommunication sc = new ServerCommunication();
+        private final FileHandling fh = new FileHandling();
+        
     }
-
-
 
 }
 
