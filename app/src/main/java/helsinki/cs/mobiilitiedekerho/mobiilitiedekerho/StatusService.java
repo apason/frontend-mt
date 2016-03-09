5d02a833c46203aebf6267ae65f112a7f8172629
Daniel <daniel.martinkarttunen@helsinki.fi>
2016-03-10 00:39:38 +0200

    Lisätty subuser muuttujat globaali-muuttuja luokkaan.

--- app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/StatusService.java
+++ app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/StatusService.java
@@ -8,10 +8,16 @@ public class StatusService {
     
         protected static final String urli = "https://mobiilitiedekerho.duckdns.org:27461/"; //The IP of the back-end server, it is needed to add parameters to it to be able to comunicate with it. Hard-coded.
     
+    
         protected static boolean loggedIn;
-        protected static String nick;
         protected static String authToken;
         
+        protected static String currentNick; //the not-unique name of the curent subuser.
+        protected static String currentSubUserID;
+    
+        protected static String[] aviableSubUsers; //The Ids of the aviable subusers for the current user. TODO ?: String[] -> ???
+    
+    
         protected static final JsonConverter jc = new JsonConverter();
         protected static final ServerCommunication sc = new ServerCommunication();
         protected static final FileHandling fh = new FileHandling();
