c3e892d90830d777d118907d07f5f79c4cd6f2f9
Vainonen <okko.vainonen@cs.helsinki.fi>
2016-04-04 16:32:22 +0300

    kovaa on koodattu

--- app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/StatusService.java
+++ app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/StatusService.java
@@ -15,8 +15,8 @@ public class StatusService {
         protected static String taskBucket = "p60v4ow30312-tasks";
         protected static String answerBucket = "p60v4ow30312-answers";
         protected static String graphicsBucket = "p60v4ow30312-graphics";
-        
 
+        //protected static final String VideoPlay_HtmlTemplate = "<!DOCTYPE html><head><script>var elem = document.getElementById(\"video\");if (elem.requestFullscreen) {elem.requestFullscreen();} else if (elem.msRequestFullscreen) {elem.msRequestFullscreen();} else if (elem.mozRequestFullScreen) {elem.mozRequestFullScreen();} else if (elem.webkitRequestFullscreen) {elem.webkitRequestFullscreen();}elem.play();</script></head><body><link href=\"//vjs.zencdn.net/5.8/video-js.min.css\" rel=\"stylesheet\"><script src=\"//vjs.zencdn.net/5.8/video.min.js\"></script><video controls id=\"video\" class=\"video-js vjs-default-skin\" width=\"100%\" height=\"100%\" preload=\"auto\" data-setup=\"{}\" autoplay><source src=\"#video_src#\"></video></body>";
         protected static final String VideoPlay_HtmlTemplate = "<!DOCTYPE html><head><script>var elem = document.getElementById(\"video\");if (elem.requestFullscreen) {elem.requestFullscreen();} else if (elem.msRequestFullscreen) {elem.msRequestFullscreen();} else if (elem.mozRequestFullScreen) {elem.mozRequestFullScreen();} else if (elem.webkitRequestFullscreen) {elem.webkitRequestFullscreen();}elem.play();</script></head><body><video controls id=\"video\" width=\"100%\" height=\"100%\" preload=\"auto\" data-setup=\"{}\" autoplay><source src=\"#video_src#\"></video></body>";
         
 
