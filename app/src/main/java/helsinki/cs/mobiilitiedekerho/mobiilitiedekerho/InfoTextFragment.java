5611f2f0279af34d260e3d3f47b0fe96e1da1380
Daniel <daniel.martinkarttunen@helsinki.fi>
2016-04-07 22:16:15 +0300

    Korjattu merge konflikti mitä ei ollut hoidettu + valittu parempi versio (ja poistettu turha logi)

--- app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/InfoTextFragment.java
+++ app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/InfoTextFragment.java
@@ -63,14 +63,7 @@ public class InfoTextFragment extends Fragment implements View.OnClickListener {
 
         boolean parsingWorked = StatusService.StaticStatusService.jc.newJson(response);
         if (parsingWorked && StatusService.StaticStatusService.sc.checkStatus()) {
-<<<<<<< HEAD
-            ArrayList<HashMap<String, String>> task = StatusService.StaticStatusService.jc.getObjects();
-            String taskInfo = task.get(0).get("info");
-=======
             String taskInfo = StatusService.StaticStatusService.jc.getProperty("Info");
-            Log.i("taskInfo", taskInfo);
-
->>>>>>> 26578a6a6a1b6e116d5389b1f0c3c1a4245dd3c0
 
             //Checks whether the task has info defined or not. If not it sets "Ei ole kuvausta tehtävälle." as the task's description.
             if (taskInfo == null) textView.setText("Ei ole kuvausta tehtävälle.");
