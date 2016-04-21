f99a8413626a6764421327e60350929438217c5a
Daniel <daniel.martinkarttunen@helsinki.fi>
2016-04-21 18:13:49 +0300

    Uusi S3Upload rakenne, joka käyttää Signed URLja ja suoraan PUT käskyä.

--- app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/S3Upload.java
+++ app/src/main/java/helsinki/cs/mobiilitiedekerho/mobiilitiedekerho/S3Upload.java
@@ -17,91 +17,67 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A class for uploading videos to S3.
+ * When calling execute pass as String... urli the signed url to be used for upload the file.
  */
 public class S3Upload extends AsyncTask<String, Void, String> {
 
     private TaskCompleted act;
     private File selectedFile;
-    private Context context;
-    
-    private String outcome = "succes";
     
     /**
     * Constructor for S3Upload.
-    * @param act a interface for being able to pass the response for the calling ativity.
+    * @param act a interface for being able to pass the response for the calling activity.
+    * @param selectedFile the file to be uploaded
     */
-    public S3Upload(TaskCompleted act, Context context, File selectedFile){
+    public S3Upload(TaskCompleted act, File selectedFile){
         this.act = act;
-        this.context = context;
         this.selectedFile = selectedFile;
     }
     
     
-    protected String doInBackground(String... selectedFileName) {
-        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-            context,
-                //"LISÄÄ TÄHÄN IDENTITY POOL ID", // Identity Pool ID
-                 // Identity Pool ID
-                "eu-west-1:f153a6ae-5412-4972-8079-ef5b11eba1c8",
-                Regions.EU_WEST_1
-                //Regions.EU_CENTRAL_1 // Region
-                //Regions.EU_WEST_1
-        );
-        // Create an S3 client
-        AmazonS3 s3 = new AmazonS3Client(credentialsProvider);
-
-        // Set the region of your S3 bucket
-        s3.setRegion(Region.getRegion(Regions.EU_CENTRAL_1));
-        
-        
-        //Sets the metadata to the file to be uploaded.
-        ObjectMetadata metadata = new ObjectMetadata();
-        
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        //TODO: There is no need for the extension part ".type" actually (except for Windows) so possibly there could be a viable file without its type written in the file's name.
-        //Also the extension may or may not tell the truth about the file's type. (No need to take into account?)
-        //In android it is possible to use "android.media.MediaMetadataRetriever" for reading the file's metadata and get the real type. TODO: Do this to the File selectedFile, Note: this is not very favored for some reason, why?
-        String ext = selectedFileName[0].substring(selectedFileName[0].lastIndexOf(".")).toLowerCase(); //toLowerCase in the (odd) case of the extension being in UpperCase, else MimeType may not recognize it.
-        String type = mime.getMimeTypeFromExtension(ext); //Gets the Mime type corresponding to the extension. E.G: mp4 -> video/mp4
-        
-        metadata.setContentType(type);
-        metadata.setContentDisposition("inline");
-
-
-        
-        TransferUtility transferUtility = new TransferUtility(s3, context);
-
-        TransferObserver observer = transferUtility.upload(
-            StatusService.StaticStatusService.answerBucket,     /* The bucket to upload to */
-            selectedFileName[0],                                /* The key for the uploaded object */
-            selectedFile,                                       /* The file where the data to upload exists */
-            metadata                                            /* The metadata (HTTP-header stuff) for the file to be uploaded */
-        );
-        Log.i("amazondata", selectedFileName[0]+" "+selectedFile+ " "+metadata.toString());
-        observer.setTransferListener(new TransferListener() {
-
-            @Override
-            public void onStateChanged(int id, TransferState state) {
-                Log.i("dev", state.name());
+    protected String doInBackground(String... urli) {
+        try {
+        	URL url = new URL(urli[0]);
+        	
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("PUT");
+            
+            //# Sets the metadata to the file to be uploaded.
+            MimeTypeMap mime = MimeTypeMap.getSingleton();
+            //TODO: There is no need for the extension part ".type" actually (except for Windows) so possibly there could be a viable file without its type written in the file's name.
+            //Also the extension may or may not tell the truth about the file's type. (No need to take into account?)
+            //In android it is possible to use "android.media.MediaMetadataRetriever" for reading the file's metadata and get the real type. TODO: Do this to the File selectedFile, Note: this is not very favored for some reason, why?
+            String ext = selectedFile.getName().substring(selectedFile.getName().lastIndexOf(".")).toLowerCase(); //toLowerCase in the (odd) case of the extension being in UpperCase, else MimeType may not recognize it.
+            String type = mime.getMimeTypeFromExtension(ext); //Gets the Mime type corresponding to the extension. E.G: mp4 -> video/mp4
+            connection.setRequestProperty("Content-Type", type); // Very important ! It won't work without adding this!
+            connection.setRequestProperty("Content-Disposition", "inline");
+            
+            BufferedOutputStream bos = new BufferedOutputStream(connection.getOutputStream());
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(selectedFile));
+            int i;
+            // read byte by byte until end of stream
+            while ((i = bis.read()) > -1) {
+                bos.write(i);
             }
-
-            @Override
-            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
-                Log.i("dev", bytesCurrent + "/" + bytesTotal + "-" + ((float) bytesCurrent / (float) bytesTotal) * 100);
-            }
-
-            @Override
-            public void onError(int id, Exception ex) {
-                Log.i("Amazon", "Error on amazon service ! " + ex);
-                outcome = "failure";
-            }
-        });
-
-        return outcome;
+            bos.close();
+            
+            connection.disconnect();
+            
+            return "succes";
+        } catch (Exception e) {
+            return "failure";
+        }
+        //TODO: Check response code, connection.getResponseCode()
     }
     
     protected void onPostExecute(String result) {
